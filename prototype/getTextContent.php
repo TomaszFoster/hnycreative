e83deb74942303b1f47465dfaa811ac367c32c28
Steve Nelson <snelson@zenmanenergy.org>
2014-06-10 16:13:27 -0400

    Added dynamically driven static text content

--- prototype/getTextContent.php
+++ prototype/getTextContent.php
@@ -14,7 +14,32 @@
     		"text1" => "Lorems ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in tortor dolor. Nunc tempus molestie odio sit amet id risus velit.",
     		
 			 ),
-    	
+    	'screen8' => array( 
+    		"text1" => "Lorems ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in tortor dolor. Nunc tempus molestie odio sit amet id risus velit.",
+   			"passwordMustMatch" => "Your password must match the requirements", 		
+			 ),
+		'screen8b' => array( 
+    		"text1" => "Lorems ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in tortor dolor. Nunc tempus molestie odio sit amet id risus velit.",
+   			 ),
+   			 
+		'screen13' => array( 
+    		"headline1" => "You've Earned a Heart!",
+    		"text1" => "We suspected you were a good person, but you've just confirmed it. Let's seal our friendship with a high five.",
+   			 ),
+   		'screen15' => array( 
+    		"headline1" => "You've Earned a Reward!",
+    		"text1" => "Congratulations on making it to the<BR> \"let's buy each other gifts\" phase <BR>of this relationship.",
+   			 ),
+   		'screen16' => array( 
+    		"title" => "Reward Detail"
+   			 ),
+   		'screen17' => array( 
+    		"filterYourResults" => "Filter Your Results"
+   			 ),
+   		'screen24' => array( 
+    		"title" => "Your Badges"
+   			 ),
+   			 
 	);
 
 	echo json_encode($textContent);
